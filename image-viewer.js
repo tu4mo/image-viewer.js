1e51aa95f3fb0ab6059af336458a368af0d6bfa4
tu4mo <tu4mo@users.noreply.github.com>
2016-07-21 11:47:47 +0300

    Convert tabs to spaces

--- image-viewer.js
+++ image-viewer.js
@@ -1,44 +1,44 @@
 (function(document) {
-	'use strict';
+  'use strict';
 
-	var imageViewerElement;
-	var imageViewerContentElement;
-	var viewerStyle = '' +
-		'<style>' +
-			'#imageViewer { background: rgba(255, 255, 255, .95); bottom: 0; cursor: pointer; display: none; left: 0; position: fixed; right: 0; top: 0; z-index: 9999; }' +
-			'#imageViewer.active { display: block; }' +
-			'#imageViewerContent { left: 50%; max-height: calc(100% - 40px); max-width: calc(100% - 40px); position: absolute; top: 50%; transform: translate(-50%, -50%); }' +
-		'</style>';
-	var viewerTemplate = '<div id="imageViewer"><img id="imageViewerContent"></div>';
+  var imageViewerElement;
+  var imageViewerContentElement;
+  var viewerStyle = '' +
+    '<style>' +
+      '#imageViewer { background: rgba(255, 255, 255, .95); bottom: 0; cursor: pointer; display: none; left: 0; position: fixed; right: 0; top: 0; z-index: 9999; }' +
+      '#imageViewer.active { display: block; }' +
+      '#imageViewerContent { left: 50%; max-height: calc(100% - 40px); max-width: calc(100% - 40px); position: absolute; top: 50%; transform: translate(-50%, -50%); }' +
+    '</style>';
+  var viewerTemplate = '<div id="imageViewer"><img id="imageViewerContent"></div>';
 
-	// Insert styles before </head>
-	document.head.insertAdjacentHTML('beforeend', viewerStyle);
+  // Insert styles before </head>
+  document.head.insertAdjacentHTML('beforeend', viewerStyle);
 
-	// Insert template before </body>
-	document.body.insertAdjacentHTML('beforeend', viewerTemplate);
+  // Insert template before </body>
+  document.body.insertAdjacentHTML('beforeend', viewerTemplate);
 
-	// Get elements
-	imageViewerElement = document.getElementById('imageViewer');
-	imageViewerContentElement = document.getElementById('imageViewerContent')
+  // Get elements
+  imageViewerElement = document.getElementById('imageViewer');
+  imageViewerContentElement = document.getElementById('imageViewerContent')
 
-	// Add closing function to viewer
-	document.getElementById('imageViewer').addEventListener('click', function() {
-		document.body.style.overflow = 'auto';
-		imageViewerElement.classList.remove('active');
-	});
+  // Add closing function to viewer
+  document.getElementById('imageViewer').addEventListener('click', function() {
+    document.body.style.overflow = 'auto';
+    imageViewerElement.classList.remove('active');
+  });
 
-	// Image click event handler
-	function imageClick(e) {
-		e.preventDefault();
-		imageViewerContentElement.src = this.href;
-		imageViewerElement.classList.add('active');
-		document.body.style.overflow = 'hidden';
-	}
+  // Image click event handler
+  function imageClick(e) {
+    e.preventDefault();
+    imageViewerContentElement.src = this.href;
+    imageViewerElement.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  }
 
-	// Add click event listeners to image links
-	Array.prototype.slice
-		.call(document.querySelectorAll('[data-image-viewer]'))
-		.forEach(function(imageLink) {
-			imageLink.addEventListener('click', imageClick);
-		});
+  // Add click event listeners to image links
+  Array.prototype.slice
+    .call(document.querySelectorAll('[data-image-viewer]'))
+    .forEach(function(imageLink) {
+      imageLink.addEventListener('click', imageClick);
+    });
 })(document);
